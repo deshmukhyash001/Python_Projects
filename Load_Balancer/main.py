deda000fca180a0d691ae1c1d099ff756488cf3a
deshmukhyash001 <130223783+deshmukhyash001@users.noreply.github.com>
2025-07-17 18:16:51 +0530

    Add files via upload

--- Load_Balancer/main.py
+++ Load_Balancer/main.py
@@ -1,12 +1,49 @@
 import socket
+import threading
 
 DECODE = 'utf-8'
 class Loadbalancer():
+    
     servers = [('172.16.82.36',3000),('172.16.82.36',3001)]
     
     def __init__(self,ip,port):
+        self.current_server = 0
         self.port = port
         self.ip = ip
+     
+    def get_next_available_server(self):
+        attempts = 0
+        total_servers = len(self.servers)
+
+        while attempts < total_servers:
+            server = self.servers[self.current_server]
+            self.current_server = (self.current_server + 1) % total_servers
+
+            try:
+                test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                test_socket.settimeout(1)  
+                test_socket.connect(server)
+                test_socket.close()
+                return server 
+            except:
+                attempts += 1
+                continue
+
+        return None 
+
+        
+    def forward(self,source,destrination):
+        try :
+            while True:
+                data = source.revc(4096)
+
+                if not data:
+                    break
+                destrination.sendall(data)
+
+        finally :
+            source.close
+            destrination.close()        
         
     def start(self):
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as cs:
@@ -19,16 +56,24 @@ class Loadbalancer():
                 client_sock,addr = cs.accept()
                 print(f"accepted connection from {addr}")
 
-                backend_ip,backend_port = self.servers[0]
+                server = self.get_next_available_server()
+
+                if not server:
+                    print("No server is available at this perticular time")
+                    client_sock.close()
+                    continue
+                
+                backend_ip, backend_port = server
+
                 
                 with socket.socket(socket.AF_INET,socket.SO_REUSEADDR) as backend_socket:
                     try :
                         backend_socket.connect((backend_ip,backend_port))
                         print(f"Connected to backend server at {backend_ip}:{backend_port}")
                         
-                        backend_socket.sendall(b"Hello from load balancer ")
-                        responce = backend_socket.recv(1024)
-                        print(f"received from backend : {responce.decode(DECODE)}")
+                        threading.Thread(target=self.forward ,args=(cs,backend_socket))
+                        threading.Thread(target=self.forward ,args=(backend_socket,cs))
+                        
                         
                     except Exception as e:
                         print(f"failed connect backend {e}")
